71227550d6a867faed01ef19ec4703384604cd85
adeliaaaa <monicadelia88@gmail.com>
2022-11-28 21:40:44 +0700

    feat: add register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,88 @@
+import './Register.css'
+import axios from 'axios'
+import { useCookies } from 'react-cookie';
+
+function Register(){
+    const [cookies, setCookie, removeCookie] = useCookies(['token']);
+    console.log(cookies)
+
+    const registerJSON = async (e) =>{
+        e.preventDefault();
+        //username, password, email, name
+        console.log("MASUK SINI")
+        const data = {
+            "username": document.getElementById("username").value,
+            "password": document.getElementById("password").value,
+            "email": document.getElementById("email").value,
+            "name": document.getElementById("name").value
+        }
+        console.log(data)
+        await axios.post('http://localhost:5000/register', data).then(response=>{
+            console.log("HALOOOOOOOOOOOOOOO")
+            console.log(response.data)
+            console.log(response)
+            setCookie("token", response.data.token)
+            console.log(cookies)
+
+        }).catch(err =>{
+            // console.log(response.data)
+            console.log("OH NO ERRORRR")
+            console.log(err.response.data.error)
+            
+        })
+    }
+
+    return(
+        <div>
+            <div class="login-background">
+                <div class="login-title-container">
+                    <a class="login-title"> BINOTIFY </a>
+                </div>
+                    <div class="login-container">
+                        <div class="login-form-container">
+                            <form method="post" class="login-form">
+                                <div class="login-input">
+                                    <label class="login-input-text" for="username" required> Username </label>
+                                    <input class="login-input-box" placeholder="Enter your username" type="text" name="username" id="username" required onkeyup="processUsernameChange(this.value)"/>
+                                    {/* <p class="register-error-msg-ajax" id="errorMsgUsername">a</p> */}
+                                </div>
+                                
+                                <div class="login-input">
+                                    <label class="login-input-text" requires> Email </label>
+                                    <input class="login-input-box" placeholder="Enter your email" type="text" name="email" id="email" required onkeyup="processEmailChange(this.value)"/>
+                                    {/* <p class="register-error-msg-ajax" id="EMAILL">b</p> */}
+                                </div>
+
+                                <div class="login-input">
+                                    <label class="login-input-text" required> Password </label>
+                                    <input class="login-input-box" placeholder="Enter your password" type="password" name="password" id="password" required/>
+                                </div>
+
+                                <div class="login-input">
+                                    <label class="login-input-text" required> Confirm Password </label>
+                                    <input class="login-input-box" placeholder="Enter your password again" type="password" name="confirm_password" required/>
+                                </div>
+
+                                <div class="login-input">
+                                    <label class="login-input-text" required> What should we call you? </label>
+                                    <input class="login-input-box" placeholder="Enter a profile name" type="text" name="name" id="name" required/>
+                                </div>
+
+                                <div class="register-button-container">
+                                    <button class="register-button" value="Submit" id="registerButton" onClick={(e) => registerJSON(e)}> SIGN UP </button>
+                                </div>
+                                
+                            </form> 
+                                <p class="register-login-button">
+                                    Have an account?
+                                    <a class="register-href" href="/login">Login</a>
+                                </p>
+
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
